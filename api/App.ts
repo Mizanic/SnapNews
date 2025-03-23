137a307c01b491398fcffddc318fea635be10e87
Rehan Haider <email@rehanhaider.com>
2025-03-23 06:54:28 +0000

    CHORE:  api framework

---
+++ api/App.ts
@@ -0,0 +1,1 @@
+import { App } from "aws-cdk-lib";
---
+++ api/src/Reader.ts
@@ -0,0 +1,16 @@
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
+
+export interface ReaderStackProps extends StackProps {
+  readerName: string;
+}
+
+
+export class ReaderStack extends Stack {
+  constructor(scope: Construct, id: string, props: ReaderStackProps) {
+    super(scope, id, props);
+
+    // Create a new RestApi
+
+  }
+}
