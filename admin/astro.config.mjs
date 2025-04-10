47c6bfd0bd01c750ee5e329adfaebd49f7673425
Rehan Haider <email@rehanhaider.com>
2025-04-10 17:31:29 +0000

    FEAT:  initial draft of admin panel

--- admin/astro.config.mjs
+++ admin/astro.config.mjs
@@ -1,15 +1,34 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from "astro/config";
 
-import react from '@astrojs/react';
+import react from "@astrojs/react";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
-  vite: {
-    plugins: [tailwindcss()]
-  }
+    integrations: [react()],
+    output: "static",
+    prefetch: {
+        prefetchAll: true,
+        defaultStrategy: "viewport",
+    },
+    vite: {
+        plugins: [tailwindcss()],
+    },
+    server: {
+        port: 7000,
+    },
+    env: {
+        schema: {
+            PUBLIC_CDK_AWS_REGION: envField.string({ context: "client", access: "public" }),
+            PUBLIC_USER_POOL_ID: envField.string({ context: "client", access: "public" }),
+            PUBLIC_APP_CLIENT_ID: envField.string({ context: "client", access: "public" }),
+            PUBLIC_RECAPATCHA_CLIENT_KEY: envField.string({
+                context: "client",
+                access: "public",
+                default: "6LfzuYwqAAAAAIPcU6sHC0ralyhfDK9xeadWHM5j",
+            }),
+        },
+    },
 });
