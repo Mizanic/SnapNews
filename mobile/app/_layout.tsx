382af6ed1f665e0d3cc129e9b2eb1479f8da73ec
Rehan Haider <email@rehanhaider.com>
2025-07-04 17:08:19 +0000

    STYLE:  linter errors

--- mobile/app/_layout.tsx
+++ mobile/app/_layout.tsx
@@ -1,56 +1,55 @@
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
-import Navigation from "@/components/Navigation";
 import "react-native-reanimated";
 
-import { useColorScheme } from "@/hooks/useColorScheme";
-import AppHeader from "@/components/screens/AppHeader";
 import { Provider } from "react-redux";
 import store, { persistor } from "../dux/store";
-import { PersistGate } from 'redux-persist/integration/react';
+import { PersistGate } from "redux-persist/integration/react";
 import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
-
+import { useColorScheme } from "hooks/useColorScheme.web";
+import AppHeader from "components/screens/AppHeader";
+import Navigation from "components/Navigation";
 
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
-    "Lora-Regular": require("../assets/fonts/Lora/static/Lora-Regular.ttf"),
-    "Lora-Bold": require("../assets/fonts/Lora/static/Lora-Bold.ttf"),
-    "Inter-Regular": require("../assets/fonts/Inter/static/Inter_18pt-Regular.ttf"),
-    "Inter-Bold": require("../assets/fonts/Inter/static/Inter_18pt-Bold.ttf"),
-  });
+    const colorScheme = useColorScheme();
+    const [loaded] = useFonts({
+        SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+        "Lora-Regular": require("../assets/fonts/Lora/static/Lora-Regular.ttf"),
+        "Lora-Bold": require("../assets/fonts/Lora/static/Lora-Bold.ttf"),
+        "Inter-Regular": require("../assets/fonts/Inter/static/Inter_18pt-Regular.ttf"),
+        "Inter-Bold": require("../assets/fonts/Inter/static/Inter_18pt-Bold.ttf"),
+    });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
     }
-  }, [loaded]);
 
-  if (!loaded) {
-    return null;
-  }
-
-  return (
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <SafeAreaProvider>
-          <SafeAreaView style={styles.container}>
-            <AppHeader />
-            <Navigation />
-          </SafeAreaView>
-        </SafeAreaProvider>
-      </PersistGate>
-    </Provider>
-  );
+    return (
+        <Provider store={store}>
+            <PersistGate loading={null} persistor={persistor}>
+                <SafeAreaProvider>
+                    <SafeAreaView style={styles.container}>
+                        <AppHeader />
+                        <Navigation />
+                    </SafeAreaView>
+                </SafeAreaProvider>
+            </PersistGate>
+        </Provider>
+    );
 }
 
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-    }
-})
+    },
+});
