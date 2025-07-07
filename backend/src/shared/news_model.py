eba9ef09f2cf9e62c47b757966421d08a3a3395b
Rehan Haider <email@rehanhaider.com>
2025-07-07 18:41:46 +0000

    FEAT:  better validations

--- backend/src/shared/news_model.py
+++ backend/src/shared/news_model.py
@@ -3,9 +3,14 @@
 # THis module defines the datamodels for the news feed
 """
 
+# ==================================================================================================
+# Python imports
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+# ==================================================================================================
+# Data models
 
 
 class NewsMediaModel(BaseModel):
@@ -13,8 +18,37 @@ class NewsMediaModel(BaseModel):
     This class defines the data model for the media object in the news feed
     """
 
-    image_url: str
-    video_url: Optional[str]
+    image_url: Optional[str] = None
+    video_url: Optional[str] = Field(default=None)
+
+
+class SourceNewsModel(BaseModel):
+    """
+    This class defines the data model for the default object in the news feed
+    """
+
+    source_name: str
+    source_id: str
+    country: str
+    language: str
+    news_url: str
+    headline: str
+    published: str
+    summary: str
+    # content: str # This will be added later. Not being used to save space in DynamoDB
+    categories: list[str]
+    media: NewsMediaModel
+
+
+class MetricsModel(BaseModel):
+    """
+    This class defines the data model for the metrics object in the news feed
+    """
+
+    views: int
+    likes: int
+    bookmarks: int
+    shares: int
 
 
 class NewsItemModel(BaseModel):
@@ -25,22 +59,16 @@ class NewsItemModel(BaseModel):
     # Computed metadata
     pk: str
     sk: str
-    item_hash: str
     ttl: int
 
-    # News content
-    source_name: str
-    source_id: str
-    category: str
-    country: str
-    language: str
-    news_url: str
-    headline: str
-    published: str
-    summary: str
-    tags: Optional[list[str]]
-    # content: str #This will be added later. Not being used to save space in DynamoDB
-    media: NewsMediaModel
+    # News default
+    news: SourceNewsModel
+
+    # News metrics
+    metrics: MetricsModel
+
+    # Computed keys
+    sk_top: str
 
 
 class NewsFeedModel(BaseModel):
