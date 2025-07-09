99c5ac90f1e634a8ec0ab4cfccb25c4880ca44bd
Rehan Haider <email@rehanhaider.com>
2025-07-09 06:11:14 +0000

    FIX:  fixed validation issues

--- backend/src/fn/reader/lib/parsers/ndtv.py
+++ backend/src/fn/reader/lib/parsers/ndtv.py
@@ -31,23 +31,26 @@ class NDTV:
         logger.info(f"Parsing feed for {category} in {language} for {country}")
 
         feed: list[SourceNewsItemModel] = []
+        item_count = 0
+        parsed_item_count = 0
         for item in xml_root.findall("./channel/item"):
-
+            item_count += 1
             try:
-                news_item = self.__parse_item(item, category, language, country)
+                parsed_item = self.__parse_item(item, category, language, country)
+                news_item = SourceNewsItemModel.model_validate(parsed_item)
                 feed.append(news_item)
-
+                parsed_item_count += 1
             except (AttributeError, ValidationError) as e:
-                logger.error(f"Error parsing feed for {category} in {language} for {country}: {e}", exc_info=e)
-                logger.error(f"Error Item: {item}")
+                logger.debug(f"Error parsing feed for {category} in {language} for {country}: {e}", exc_info=e)
+                logger.debug(f"Error Item: {item}")
                 # In future, we can identify the exact error and put in a queue for scraping
                 continue
 
-        logger.info(f"Parsed {len(feed)} items for {category} in {language} for {country}")
+        logger.info(f"Parsed {parsed_item_count} items out of {item_count} for {category} in {language} for {country}")
 
         return SourceNewsFeedModel(feed=feed)
 
-    def __parse_item(self, item: Element, category: str, language: str, country: str) -> SourceNewsItemModel:
+    def __parse_item(self, item: Element, category: str, language: str, country: str) -> dict:
         """
         This function parses an a single XML item and returns a SourceNewsItemModel
         """
@@ -67,7 +70,7 @@ class NDTV:
         if clean_summary == "":
             clean_summary = None
 
-        params = {
+        data = {
             "source_name": "NDTV",
             "source_id": "NDTV",
             "country": country,
@@ -79,20 +82,6 @@ class NDTV:
             "categories": [category],
             "media": media,
         }
-
-        logger.info(f"Params: {params}")
-
-        data = SourceNewsItemModel(
-            source_name="NDTV",
-            source_id="NDTV",
-            country=country,
-            language=language,
-            news_url=item.findtext("link"),
-            headline=item.findtext("title"),
-            published=time_to_iso(item.findtext("pubDate")),
-            summary=clean_summary,
-            categories=[category],
-            media=media,
-        )
+        logger.info(f"Data: {data}")
 
         return data
