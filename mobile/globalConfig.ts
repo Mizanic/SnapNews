3653230d38f1fd1b2d33f527359038b55d304dc0
Rehan Haider <email@rehanhaider.com>
2025-08-05 04:51:07 +0000

    CHORE:  fixed urls

--- mobile/globalConfig.ts
+++ mobile/globalConfig.ts
@@ -1,4 +1,5 @@
-export const TELEMETRY_API_URL = "/api/like";
+export const API_ENDPOINT = "https://5695pjsso7.execute-api.us-east-1.amazonaws.com/v1";
+export const TELEMETRY_API_URL = "/actions";
 export const BATCH_SIZE = 3;
 
 // SQLite Database Configuration
