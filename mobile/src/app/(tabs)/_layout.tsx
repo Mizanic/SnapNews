cea84e1c0ee666aa2d8899e7f98cf1aee76e8aa1
Rehan Haider <email@rehanhaider.com>
2025-08-07 07:13:31 +0000

    STYLE:  moved tab bar to a layout

--- mobile/src/app/(tabs)/_layout.tsx
+++ mobile/src/app/(tabs)/_layout.tsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
-import { View, StyleSheet, Platform, TouchableOpacity } from "react-native";
-import { MaterialCommunityIcons, Ionicons, MaterialIcons } from "@expo/vector-icons";
-import { BlurView } from "expo-blur";
+import { View, Platform } from "react-native";
 import TopNewsScreen from "./top";
 import LatestNewsScreen from "./index";
 import BookmarkScreen from "./bookmarks";
-import { Spacing, Shadows, BorderRadius } from "@/styles";
+import { Shadows, BorderRadius } from "@/styles";
 import { useThemeColors } from "@/hooks/useThemeColor";
 import useColorScheme from "@/hooks/useColorScheme.web";
 import AppHeader from "@/components/layout/AppHeader";
@@ -33,28 +31,7 @@ const MainTabs: React.FC = () => {
                 initialRouteName="Latest News"
                 tabBarPosition="bottom"
                 screenOptions={{
-                    tabBarStyle: {
-                        height: 60 + insets.bottom,
-                        paddingBottom: insets.bottom,
-                        backgroundColor: Platform.OS === "ios" ? "transparent" : colors.backgroundColors.primary,
-                        borderTopWidth: 0,
-                        borderTopLeftRadius: BorderRadius.xl,
-                        borderTopRightRadius: BorderRadius.xl,
-                        position: "absolute",
-                        ...Shadows.lg,
-                        ...Platform.select({
-                            ios: {
-                                shadowColor: colors.black,
-                                shadowOffset: { width: 0, height: -8 },
-                                shadowOpacity: 0.1,
-                                shadowRadius: 16,
-                            },
-                            android: {
-                                elevation: 8,
-                            },
-                        }),
-                    },
-                    tabBarShowLabel: false,
+                    tabBarShowLabel: true,
                     tabBarIndicatorStyle: { height: 0 }, // Hide the top indicator
                     swipeEnabled: true,
                 }}
